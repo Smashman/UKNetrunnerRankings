2e12ec25da2a462566abf7d188dc856e6577cfa4
Ben <ben@smash.mn>
2016-02-01 21:53:06 +0000

    Commit a example settings file and a coming soon message

---
+++ instance/settings_example.py
@@ -0,0 +1,3 @@
+SECRET_KEY = ''  # Key for forms (can be anything)
+SQLALCHEMY_DATABASE_URI = ''  # URI for SQL Database
+UPLOADED_TOURNAMENTEXPORTS_DEST = ''  # Location for uploaded tournaments to be saved
